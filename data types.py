fd2fc51d19c8822c085ec2226c39007ecdf98c22
Nathnael Yohannes <nati10yo123@gmail.com>
2025-06-27 00:02:36 +0300

    my last commit of the day on python data types, date:6/27/25

--- data types.py
+++ data types.py
@@ -7,3 +7,8 @@ e = "23"
 f = int(e)
 print("The number " + d + " is an Odd number!")
 print(70 + f)
+tdn = input("Type a two digit number: ")
+fd = tdn[0]
+sd = tdn[1]
+outcome = int(fd) + int(sd)
+print(outcome)
